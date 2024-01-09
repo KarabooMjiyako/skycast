b0b811db0a5c50bec9e754ac625ecd6d2ce4bb46
Karabo Mjiyako <karabomjiyako@icloud.com>
2024-01-09 02:38:07 +0200

    "corrected the netlify link"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <Weather defaultCity="Randfontein"/>
       <footer>
-        This project was coded by <a href="https://github.com/KarabooMjiyako" target="_blank" rel="noopener noreferrer"> Karabo Mjiyako</a> and is <a href="https://github.com/KarabooMjiyako/skycast" target='_blank' rel="noopener noreferrer">open-sourced</a> on GitHub and hosted on <a href="https://app.netlify.com/sites/celebrated-cassata-19963b/deploys/659c9262b7ff7f00088ac494" target='_blank' rel="noopener noreferrer">Netlify</a> .
+        This project was coded by <a href="https://github.com/KarabooMjiyako" target="_blank" rel="noopener noreferrer"> Karabo Mjiyako</a> and is <a href="https://github.com/KarabooMjiyako/skycast" target='_blank' rel="noopener noreferrer">open-sourced</a> on GitHub and hosted on <a href="https://celebrated-cassata-19963b.netlify.app" target='_blank' rel="noopener noreferrer">Netlify</a> .
       </footer>
     </div>
   );
