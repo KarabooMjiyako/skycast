f5bf1b04824fbdf4f83e5910d627c8598e47b495
Karabo Mjiyako <karabomjiyako@icloud.com>
2024-01-09 02:31:31 +0200

    "added relnoopener"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Weather from './Weather';
 import './App.css';
 
@@ -6,7 +7,7 @@ export default function App() {
     <div className="App">
       <Weather defaultCity="Randfontein"/>
       <footer>
-        This project was coded by <a href="https://github.com/KarabooMjiyako" target="_blank"> Karabo Mjiyako</a> and is <a href="https://github.com/KarabooMjiyako/skycast" target='_blank' >open-sourced</a> on GitHub and hosted on <a href="#">Netlify</a> .
+        This project was coded by <a href="https://github.com/KarabooMjiyako" target="_blank" rel="noopener noreferrer"> Karabo Mjiyako</a> and is <a href="https://github.com/KarabooMjiyako/skycast" target='_blank' rel="noopener noreferrer">open-sourced</a> on GitHub and hosted on <a href="#" target='_blank' rel="noopener noreferrer">Netlify</a> .
       </footer>
     </div>
   );
