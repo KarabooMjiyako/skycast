0141cbfcccd03ed0203ffa2d7e2f3567e7f2d172
Karabo Mjiyako <karabomjiyako@icloud.com>
2024-01-09 02:04:55 +0200

    "more improvements"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <Weather defaultCity="Randfontein"/>
       <footer>
-        This project was coded by <a href="#" target="_blank" >Karabo Mjiyako</a> and is <a href="#" >open-sourced</a> on GitHub.
+        This project was coded by <a href="#" target="_blank" >Karabo Mjiyako</a> and is <a href="https://github.com/KarabooMjiyako/skycast" target='_blank' >open-sourced</a> on GitHub.
       </footer>
     </div>
   );
