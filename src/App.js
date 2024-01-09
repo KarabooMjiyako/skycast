fd00ceeadac49f96e393ba4b6737ae569e5ab14a
Karabo Mjiyako <karabomjiyako@icloud.com>
2024-01-09 02:14:20 +0200

    "run build"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <Weather defaultCity="Randfontein"/>
       <footer>
-        This project was coded by <a href="#" target="_blank" >Karabo Mjiyako</a> and is <a href="https://github.com/KarabooMjiyako/skycast" target='_blank' >open-sourced</a> on GitHub.
+        This project was coded by <a href="https://github.com/KarabooMjiyako" target="_blank"> Karabo Mjiyako</a> and is <a href="https://github.com/KarabooMjiyako/skycast" target='_blank' >open-sourced</a> on GitHub and hosted on <a href="#">Netlify</a> .
       </footer>
     </div>
   );
